dd4a4811174c19365b874af2038335422c1e6c69
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-09 17:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length-1; i++){
+		for(let j=i+1; j<=str.length-1; j++){
+			if(str[i]===str[j]){
+			  str = str.replaceAll(str[j]," ")
+			}
+		}
+	} 
+	for(let i=0; i<=str.length-1; i++){
+		if(str[i]===" "){
+			str = str.replace(/^\s+$/,"")
+		}
+	}
+	if(str===""){
+		return "null"
+	}else{
+		return str
+	}
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
